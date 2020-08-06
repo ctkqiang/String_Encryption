0b0b1cc3cb758d39c2f8e331aceff34c62b4827f
John Melody Me <Johnmelodyme@yandex.com>
2020-08-06 22:51:20 +0800

    Initial Commit

---
+++ String Encryption/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  String Encryption
+//
+//  Created by John Melody Me on 06/08/2020.
+//  Copyright © 2020 John Melody Me. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
