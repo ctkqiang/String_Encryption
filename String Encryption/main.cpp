4477cd0827a790475fcb14d998af8da1ad68d19a
John Melody Me <Johnmelodyme@yandex.com>
2020-08-06 23:03:26 +0800

    Update main.cpp

--- String Encryption/main.cpp
+++ String Encryption/main.cpp
@@ -1,5 +1,3 @@
-//
-//  main.cpp
 //  String Encryption
 //
 //  Created by John Melody Me on 06/08/2020.
@@ -7,9 +5,28 @@
 //
 
 #include <iostream>
+#include <string>
+
+std::string encrypt(std::string input, std::string key) {
+    std::string tmp(key);
+    while (key.size() < input.size()){
+       key += tmp;
+    }
+    
+    for (std::string::size_type i = 0; i < input.size(); ++i){
+        input[i] ^= key[i];
+    }
+    return input;
+}
+
+std::string decrypt(std::string encrypted, std::string key) {
+    return encrypt(encrypted, key);
+}
+
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    std::string message = encrypt("Teshgfhgfhgfhfhfhgfting", "THISISMYKEY");
+    std::cout << "Encrypted: " << message;
+    std::cout << "\nDecrypted: " << decrypt(message, "THISISMYKEY");
     return 0;
 }
